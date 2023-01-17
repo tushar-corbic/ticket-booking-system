a730bb4297de5e37a8d5bcc0ee7fbbeea0ff672b
Tushar Gupta <kshitizgupta108@gmail.com>
2023-01-17 21:17:45 +0530

    added

--- models.py
+++ models.py
@@ -13,6 +13,7 @@ class AdminLoginForm(FlaskForm):
 
 # ading contirubtuio fo the new amoooit 
 #addign another contibution
+# amking anothe commit
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username","class":"form-control my-1 "})
